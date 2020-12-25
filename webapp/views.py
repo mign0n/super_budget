ce34caf374d5e27c9dbf6d4d14f4c3b246e563a6
mign0n <56875363+mign0n@users.noreply.github.com>
2020-12-25 14:08:32 +0500

    Added login form and page.

--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,16 @@
 from flask import render_template
 from webapp import app
+from webapp.forms import LoginForm
 
 
 @app.route('/')
 def index():
     title = "Welcome to the Super Budget!"
     return render_template('index.html', title=title)
+
+
+@app.route('/login')
+def login():
+    title = "Authorization"
+    login_form = LoginForm()
+    return render_template('login.html', title=title, form=login_form)
