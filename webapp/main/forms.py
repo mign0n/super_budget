6b0d5bb02818334e6bb4ce589aba4b0d58d921bd
mign0n <56875363+mign0n@users.noreply.github.com>
2021-01-13 17:44:04 +0500

    Added minimal processing of the transaction form.

---
+++ webapp/main/forms.py
@@ -0,0 +1,37 @@
+from flask_wtf import FlaskForm
+from wtforms import DateField, SelectField, StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class TransactionForm(FlaskForm):
+    value = StringField(validators=[DataRequired()],
+                        render_kw={"class": "form-control",
+                                   "placeholder": "Money value"
+                                   }
+                        )
+    date = DateField(validators=[DataRequired()],
+                     format='%d.%m.%Y',
+                     render_kw={"class": "form-control",
+                                "placeholder": "dd.mm.yyyy"
+                                }
+                     )
+    comment = StringField(render_kw={"class": "form-control",
+                                     "placeholder": "Comment"
+                                     }
+                          )
+    submit = SubmitField('Ok', render_kw={"class": "btn btn-primary"})
+    is_income = SelectField(choices=[('', 'Expense'), (True, 'Income')],
+                            coerce=bool,
+                            render_kw={"class": "btn btn-primary dropdown-toggle",
+                                       "data-toggle": "dropdown",
+                                       "aria-haspopup": "true",
+                                       "aria-expanded": "false"
+                                       }
+                            )
+    category = SelectField(choices=['Cat 1', 'Cat 2', 'Add new category...'],
+                           render_kw={"class": "btn btn-primary dropdown-toggle",
+                                      "data-toggle": "dropdown",
+                                      "aria-haspopup": "true",
+                                      "aria-expanded": "false"
+                                      }
+                           )
--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, redirect, render_template, url_for
+from flask import Blueprint, flash, redirect, render_template, url_for
 from flask_login import current_user
 
+from webapp.main.forms import TransactionForm
 
 bp = Blueprint('main', __name__, url_prefix=None)
 
@@ -11,4 +12,20 @@ def index():
         return redirect(url_for('user.login'))
     username = current_user.name
     title = f"Welcome {username}!"
-    return render_template('main/index.html', title=title)
+    form = TransactionForm()
+    return render_template('main/index.html', title=title, form=form)
+
+
+@bp.route('/transaction', methods=['POST'])
+def transaction():
+    form = TransactionForm()
+    is_income = form.is_income.data
+    value = form.value.data
+    category = form.category.data
+    date = form.date.data
+    comment = form.comment.data
+
+    flash(f"Data was write successfully. "
+          f"Is income: {is_income}; money: {value}; "
+          f"category: {category}; date: {date}; {comment}")
+    return redirect(url_for('main.index'))
