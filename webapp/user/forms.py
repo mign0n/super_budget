f0726cb587f1d65d800b7a53baa6aeac9f7ff496
1cloud11 <kot.vladislav@ukr.net>
2021-01-23 10:41:53 +0200

    Функция добавления категории, форма для добвления категорий, переписаны валидаторы username и useremail. Не доделан валидатор для новых категорий

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, PasswordField, SubmitField
+from wtforms import BooleanField, StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
-
+from flask_login import current_user
 from webapp.user.models import User
-
-from sqlalchemy.sql import exists
+from webapp.main.models import Category
+from sqlalchemy.sql import exists, or_, and_
 
 
 class LoginForm(FlaskForm):
@@ -46,11 +46,29 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Register!', render_kw={"class": "btn btn-primary"})
 
     def validate_username(self, name):
-        users_count = User.query.filter_by(name=name.data).count()
-        if users_count > 0:
+        user_exist = db.session.query(exists().where(User.name == name.data)).scalar()
+        if user_exist:
             raise ValidationError('Пользователь с таким именем уже зарегистрирован')
 
     def validate_email(self, email):
-        users_count = User.query.filter_by(email=email.data).count()
-        if users_coutn > 0:
+        user_exist = db.session.query(exists().where(User.email == email.data)).scalar()
+        if user_exist:
             raise ValidationError('Пользователь с такой почтой уже зарегистрирован')
+
+
+class CategoryForm(FlaskForm):
+    new_category = StringField('Новая категория', validators=[DataRequired()],
+                               render_kw={"class": "form-control",
+                                          "placeholder": "Название категории"})
+
+    is_income = SelectField(u'Тип категории', choices=[('1', 'Доход'), ('0', 'Расход')],
+                            render_kw={"class": "form-select",
+                                       "aria-label": "Default select example"})
+
+    submit = SubmitField('Добавить', render_kw={"class": "btn btn-primary"})
+
+    #def validate_category(self, new_category):
+        #category_exist = db.session.query.filter(and_(Category.name == new_category.data, User.id == current_user.id)).first()
+        #category_exist = db.query(User.query.filter(Category.name == new_category.data).filter(User.id == current_user.user_id).exists()).scalar()
+        #if category_exist:
+            #raise ValidationError('У вас уже есть такая категория!')
