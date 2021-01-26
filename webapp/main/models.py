4a1c4487960a8795d6186dabc396949b0f65844b
1cloud11 <kot.vladislav@ukr.net>
2021-01-26 17:00:08 +0200

    Отсортировал импорты, исправил валидатор, одалил символ u. Дополнительно удалил аргумент unique из описания таблицы Category колонки name, дропнул миграции, и создал новую

--- webapp/main/models.py
+++ webapp/main/models.py
@@ -6,7 +6,7 @@ class Category(db.Model):
     __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(User.id))
-    name = db.Column(db.String(50), nullable=False, unique=True)
+    name = db.Column(db.String(50), nullable=False)
     is_income = db.Column(db.Boolean, nullable=False)
 
     transactions = db.relationship('Transaction', backref='trans_cat', lazy='dynamic')
--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -1,10 +1,12 @@
-from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, PasswordField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from flask_login import current_user
-from webapp.user.models import User
+from flask_wtf import FlaskForm
+from sqlalchemy.sql import and_, exists
+from wtforms import BooleanField, PasswordField, SelectField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+from webapp.db import db
 from webapp.main.models import Category
-from sqlalchemy.sql import exists, or_, and_
+from webapp.user.models import User
 
 
 class LoginForm(FlaskForm):
@@ -61,14 +63,13 @@ class CategoryForm(FlaskForm):
                                render_kw={"class": "form-control",
                                           "placeholder": "Название категории"})
 
-    is_income = SelectField(u'Тип категории', choices=[('1', 'Доход'), ('0', 'Расход')],
+    is_income = SelectField('Тип категории', choices=[('1', 'Доход'), ('0', 'Расход')],
                             render_kw={"class": "form-select",
                                        "aria-label": "Default select example"})
 
     submit = SubmitField('Добавить', render_kw={"class": "btn btn-primary"})
 
-    #def validate_category(self, new_category):
-        #category_exist = db.session.query.filter(and_(Category.name == new_category.data, User.id == current_user.id)).first()
-        #category_exist = db.query(User.query.filter(Category.name == new_category.data).filter(User.id == current_user.user_id).exists()).scalar()
-        #if category_exist:
-            #raise ValidationError('У вас уже есть такая категория!')
+    def validate_new_category(self, new_category):
+        category_exist = db.session.query(exists().where(and_(Category.name == new_category.data, Category.user_id == current_user.id))).scalar()
+        if category_exist:
+            raise ValidationError('У вас уже есть такая категория!')
