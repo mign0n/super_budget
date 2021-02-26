2617ab2196b7f052145f10289bf92f8263c01283
mign0n <56875363+mign0n@users.noreply.github.com>
2021-02-26 21:53:26 +0500

    Added dash app initialization.

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,3 +1,5 @@
+import dash_html_components as html
+from dash import Dash
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -10,24 +12,29 @@ from webapp.user.views import bp as user_bp
 
 
 def create_app():
-    app = Flask(__name__)
-    app.config.from_pyfile('config.py')
-    db.init_app(app)
-    migrate = Migrate(app, db)
+    flask_app = Flask(__name__)
+    flask_app.config.from_pyfile('config.py')
+    db.init_app(flask_app)
+    migrate = Migrate(flask_app, db)
 
     login_manager = LoginManager()
-    login_manager.init_app(app)
+    login_manager.init_app(flask_app)
     login_manager.login_view = 'user.login'
 
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(user_id)
 
-    app.register_blueprint(admin_bp)
-    app.register_blueprint(main_bp)
-    app.register_blueprint(user_bp)
+    flask_app.register_blueprint(admin_bp)
+    flask_app.register_blueprint(main_bp)
+    flask_app.register_blueprint(user_bp)
 
-    return app
+    dash_app = Dash(__name__, server=flask_app,
+                    routes_pathname_prefix='/board/')
+    dash_app.layout = html.Div(children='Dashboard is coming soon.')
+    application = dash_app.server
+
+    return application
 
 
 app = create_app()
